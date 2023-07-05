67cd7f6df87025d4551c7a0df2e6df3b22b60831
Ko-Khan <111284975+Ko-Khan@users.noreply.github.com>
2023-07-05 23:22:35 +0800

    Floating Platform
    Created Collapsing Platform blocks that float in the air that falls when player steps on them.

--- Proto/Assets/CollapsingBlock.cs
+++ Proto/Assets/CollapsingBlock.cs
@@ -4,22 +4,27 @@ using UnityEngine;
 
 public class CollapsingBlock : MonoBehaviour
 {
+    private Rigidbody2D platform;
+    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        platform = GetComponent<Rigidbody2D>();
+        platform.Sleep();
+        animator = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
     private void OnCollisionEnter2D(Collision2D collision) 
     {
+
         Invoke("collapse", 3);
     }
 
     private void collapse() 
     {
-        //GetComponent<Collider2D>.enabled = false;
-        //GetComponent<SpriteRenderer>.enabled = false;
-        Destroy(gameObject);
+        animator.SetTrigger("Off");
+        platform.WakeUp();
     }
 }

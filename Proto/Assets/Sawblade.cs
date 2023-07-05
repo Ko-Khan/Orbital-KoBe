d1d736f7f34845d8572c32ea451b23da4980060a
bennyLCK <e0969875@u.nus.edu>
2023-07-05 23:39:24 +0800

    Imported additional trap art assets and implemented sawblade trap

---
+++ Proto/Assets/Sawblade.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sawblade : MonoBehaviour
+{
+
+    public bool trapActive;
+    public int trapDamage;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Player")) {
+            if (trapActive) {
+                StartCoroutine(damagePlayer(other.gameObject));        
+            }
+        }
+    }
+
+    private IEnumerator damagePlayer(GameObject player) {
+        player.GetComponent<Health>().TakeDamage(trapDamage);
+        yield return new WaitForSeconds(0.5f);
+        if (player.GetComponent<BoxCollider2D>().IsTouching(GetComponent<BoxCollider2D>())) {
+            StartCoroutine(damagePlayer(player));
+        }
+    }
+}

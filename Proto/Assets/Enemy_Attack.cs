0eee9405fd085e5a7d53bdb8483313e20d846fb2
Ko-Khan <111284975+Ko-Khan@users.noreply.github.com>
2023-07-05 21:03:39 +0800

    Generic Enemy Attack Script
    Enemy_Attack

--- Proto/Assets/Enemy_Attack.cs
+++ Proto/Assets/Enemy_Attack.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class Enemy_Attack : MonoBehaviour
 {
     public Animator animator;
+    public int attackCooldown;
+    public int damage;
+    public float range;
+    public GameObject Player;
+    public LayerMask playerLayer;
+    public Transform attackPoint;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,17 +21,31 @@ public class Enemy_Attack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+    
+    }
+
+    void OnTriggerStay2D(Collider2D other) 
+    {
         
+      StartCoroutine(Attack());
+    
     }
 
-    void onCollisionEnter2D(Collision2D collision) 
+    IEnumerator Attack()
     {
-        Debug.Log("yarr");
-      Attack();
-    }
-
-    void Attack()
-    {
+        yield return new WaitForSeconds(attackCooldown);
         animator.SetTrigger("Attack");
+        yield return new WaitForSeconds(1);
+        if (hitPlayer()) 
+        {
+          Player.GetComponent<Health>().TakeDamage(damage);
+        }
+
+    }
+
+    bool hitPlayer() {
+     Collider2D hit = Physics2D.OverlapCircle(attackPoint.position, range, playerLayer);
+        return hit != null;
+
     }
 }

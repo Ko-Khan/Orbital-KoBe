03af4d7d3e0a1341dbedce8be6554f3d108b0663
Ko-Khan <111284975+Ko-Khan@users.noreply.github.com>
2023-07-06 18:06:07 +0800

    Bugfixes for attack script
    Fixed bugs for enemy melee attacks in MeleeEnemy script

---
+++ Proto/Assets/Scripts/MeleeEnemy.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeEnemy : MonoBehaviour
+{
+    [SerializeField] private float attackCoolDown;
+    [SerializeField] private int damage; 
+    [SerializeField] private float range;
+    [SerializeField] private BoxCollider2D boxCollider;
+    [SerializeField] private LayerMask playerLayer;
+    private Health playerHealth;
+    private float cooldownTimer = Mathf.Infinity;
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cooldownTimer += Time.deltaTime;
+        if (PlayerInSIght()) 
+        {
+            if (cooldownTimer >= attackCoolDown) 
+            {
+               cooldownTimer = 0;
+               anim.SetTrigger("Attack");
+               DamagePlayer();
+            }
+        }
+    }
+
+    private bool PlayerInSIght() 
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x, boxCollider.bounds.size, 0, Vector2.left,
+        0, playerLayer);
+
+        if (hit.collider != null) 
+        {
+            playerHealth = hit.transform.gameObject.GetComponent<Health>();
+        }
+        
+        return hit.collider != null;
+        
+
+    }
+
+    private void OnDrawGizmos() 
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * range * transform.localScale.x, boxCollider.bounds.size);
+    }
+
+    private void DamagePlayer() 
+    {
+        if (PlayerInSIght())
+        {
+            playerHealth.TakeDamage(damage);
+
+        }
+    }
+}

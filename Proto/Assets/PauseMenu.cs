88b4fcafcb9ac4276e706d57e8a6d83c0e7194ac
Ko-Khan <111284975+Ko-Khan@users.noreply.github.com>
2023-07-22 00:40:04 +0800

    Implemented a pause menu where volume can be adjusted

---
+++ Proto/Assets/PauseMenu.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public bool Paused;
+
+    public GameObject pauseMenuUI;
+
+     public AudioSource music;
+
+    public Slider musicSlider;
+
+    public Slider sfxSlider;
+
+    public AudioSource SFX;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Paused = false;
+
+        music.volume = 0.2f;
+
+        musicSlider.value = 0.2f;
+
+        SFX.volume = 0.8f;
+
+        sfxSlider.value = 0.8f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+
+            if (Paused)
+            {
+                Resume();
+            } else {
+                Pause();
+            }
+
+        }
+    }
+
+    void Pause() 
+    {
+        pauseMenuUI.SetActive(true);
+
+        Time.timeScale = 0f;
+
+        Paused = true;
+
+        
+
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+
+        Time.timeScale = 1.0f;
+
+        Paused = false;
+    }
+
+    public void loadMenu()
+    {
+    Debug.Log("Menu!");
+    }
+
+    public void Quit()
+    {
+        
+    }
+
+     public void SetVolumeMusic()
+   { 
+    //   audioMixer.SetFloat("volume", volume);
+     music.volume = musicSlider.value;
+   }
+
+   public void SetVolumeSFX()
+   {
+     SFX.volume = sfxSlider.value;
+   }
+
+
+}

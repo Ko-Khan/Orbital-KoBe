f0b1ce2e4b3cfb58600db70ee183fb25a07a3def
Ko-Khan <111284975+Ko-Khan@users.noreply.github.com>
2023-07-21 12:39:35 +0800

    Portal BugFix

--- Proto/Assets/Portal.cs
+++ Proto/Assets/Portal.cs
@@ -11,22 +11,29 @@ public class Portal : MonoBehaviour
     private GameObject player;
     
     private Transform destination;
+
+    public float waitTime;
     
     [SerializeField] private float distance;
 
     [SerializeField] private GameObject Destination;
+
+    [SerializeField] private float Cooldown;
     
     void Start()
     {
         player = GameObject.FindWithTag("Player");
         
         destination = Destination.transform;
+
+        waitTime = 0;
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (Vector2.Distance(other.transform.position, transform.position) > distance)
+        if (Vector2.Distance(other.transform.position, transform.position) > distance && Time.time > waitTime)
         {
+            Destination.GetComponent<Portal>().waitTime = Time.time + Cooldown;
             GameObject.FindWithTag("Audio").GetComponent<AudioManager>().PlaySFX("Portal");
             player.transform.position = new Vector2(destination.position.x , destination.position.y);
         }

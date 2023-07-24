12fa621f428e05562081f7389456124f3734ed8a
bennyLCK <e0969875@u.nus.edu>
2023-07-24 11:58:24 +0800

    fixed scene transition bug in game

--- Proto/Assets/NextLevel.cs
+++ Proto/Assets/NextLevel.cs
@@ -5,7 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class NextLevel : MonoBehaviour
 {
-    [SerializeField] private string nextLevelName;
     // Start is called before the first frame update
     void Start()
     {
